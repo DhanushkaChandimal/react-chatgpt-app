0527b01865bfe6b20fd5c1446a6f0016c7212721
Dhanushka <dhanushkamcr@gmail.com>
2025-08-31 20:17:22 -0500

    Add component to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,25 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import Sidebar from './components/Sidebar'
+import ChatWindow from './components/ChatWindow'
+import MessageInput from './components/MessageInput'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [messages, setMessages] = useState([])
+  const chats = ['Chat 1', 'Help with something', 'Cake receipe']
+
+  const handleSend = (text) => {
+    setMessages(prev => [...prev, { role: 'user', text}])
+  }
 
   return (
-    <>
-      <Sidebar chats = {["asdasd", "dsadas"]}/>
-    </>
+    <div className='app-container'>
+      <Sidebar chats = {chats}/>
+      <div className='chat-area'>
+        <ChatWindow messages={messages}/>
+        <MessageInput onSend={handleSend}/>
+      </div>
+    </div>
   )
 }
 
